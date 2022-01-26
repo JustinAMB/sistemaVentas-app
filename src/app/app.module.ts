d4f85761ec0b6f8954d1b1e6c4136015972dae83
JustinAMB <Justinmb@outlook.es>
2022-01-25 21:51:01 -0600

    se realiza la maquetacion del componente de la pagina de error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
