ed70cc17cb649ec673db633a9f103afe899b0289
JustinAMB <Justinmb@outlook.es>
2022-01-25 22:19:39 -0600

    se realiza la interface para los products

---
+++ src/app/interfaces/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id?:number;
+    name:string;
+    image?:string;
+    description:string;
+    barcode:string;
+    price:number;
+    inventary_min:number;
+    inventary:number;
+    category:number;
+    unit:string;
+}
