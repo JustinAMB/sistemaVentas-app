c53b9011edb10f0db49b2f36c86743adf7a53a9b
JustinAMB <Justinmb@outlook.es>
2022-01-26 22:09:47 -0600

    se realiza un servicio para hacer las peticiones http de person(cliente o proveedor)

---
+++ src/app/services/person.service.ts
@@ -0,0 +1,49 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Person } from '../interfaces/person';
+import { Resp } from '../interfaces/resp';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonService {
+
+  private baseUrl:string=environment.api;
+  constructor(private http:HttpClient) { }
+  
+
+
+  get token():string{
+    return localStorage.getItem('token') || '';
+  }
+  get headers(){
+    return{
+      headers:{
+        'x-access-token':this.token
+      }
+    }
+  }
+
+
+  addPerson(kind:number,person:Person):Observable<Resp>{
+    const url=`${this.baseUrl}person/${kind}`;
+    return this.http.post<Resp>(url,{...person},this.headers);
+
+  }
+  editPerson(id:number,person:Person):Observable<Resp>{
+    const url=`${this.baseUrl}person/${id}`;
+    return this.http.put<Resp>(url,{...person},this.headers);
+
+  }
+  
+  getPerson(id:number):Observable<Resp>{
+    const url=`${this.baseUrl}person/${id}`;
+    return this.http.get<Resp>(url,this.headers);
+  }
+  getPersons(kind:number):Observable<Resp>{
+    const url=`${this.baseUrl}person/getPersons/${kind}`;
+    return this.http.get<Resp>(url,this.headers);
+  }
+}
