d624f935531465f647ccb245f9b694b1a7ccd173
JustinAMB <Justinmb@outlook.es>
2022-01-28 01:35:23 -0600

    se importa el modulo de pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 import { SharedModule } from './shared/shared.module';
+import { PagesModule } from './pages/pages.module';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule,SharedModule
+    AppRoutingModule,SharedModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
