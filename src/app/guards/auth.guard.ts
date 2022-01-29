5dfed241ed6cd13b217c2a89b09b909b968b77c4
JustinAMB <Justinmb@outlook.es>
2022-01-29 01:47:25 -0600

    se realiza un guard para verificar la autenticacion

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate, CanLoad {
+  constructor(private router:Router,private authService:AuthService){}
+  canLoad(route: Route, segments: import("@angular/router").UrlSegment[]): boolean | import("@angular/router").UrlTree | import("rxjs").Observable<boolean | import("@angular/router").UrlTree> | Promise<boolean | import("@angular/router").UrlTree> {
+    return this.authService.validateToken()
+        .pipe(
+          tap( estaAutenticado =>  {
+            if ( !estaAutenticado ) {
+              this.router.navigateByUrl('/login');
+            }
+          })
+        );
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot) {
+
+      return this.authService.validateToken()
+        .pipe(
+          tap( estaAutenticado =>  {
+            if ( !estaAutenticado ) {
+              this.router.navigateByUrl('/login');
+            }
+          })
+        );
+  }
+}
