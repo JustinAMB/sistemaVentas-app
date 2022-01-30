9a8b3863e4431aeb7a5ea39dd64212118e657027
JustinAMB <Justinmb@outlook.es>
2022-01-30 02:07:46 -0600

    se definen las rutas del modulo

---
+++ src/app/category/category-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormCategoryComponent } from './form-category/form-category.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component:HomeComponent
+  },{
+    path:'editar-categoria/:id',
+    component:FormCategoryComponent
+  },{
+    path:'agregar-categoria',
+    component:FormCategoryComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class CategoryRoutingModule { }
