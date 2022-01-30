315b9c859d98fc1386ca34b40ad88503500564a1
JustinAMB <Justinmb@outlook.es>
2022-01-30 02:04:33 -0600

    se realiza la funcion activeCategory

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { delay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Category } from '../interfaces/category';
 import { Resp } from '../interfaces/resp';
@@ -35,15 +36,23 @@ export class CategoryService {
     return this.http.put<Resp>(url,{...category},this.headers);
 
   }
+  activeCategory(id:number,state:boolean):Observable<Resp> {
+    const url=`${this.baseUrl}category/active/${id}`; 
+    return this.http.put<Resp>(url,{state},this.headers);
+
+  }
 
   getCategory(id:number):Observable<Resp> {
     const url=`${this.baseUrl}category/${id}`; 
     return this.http.get<Resp>(url,this.headers);
 
   }
-  getCategorys():Observable<Resp> {
-    const url=`${this.baseUrl}category/`; 
-    return this.http.get<Resp>(url,this.headers);
+  getCategorys(state:boolean):Observable<Resp> {
+
+    const url=`${this.baseUrl}category/?state=${state}`; 
+    return this.http.get<Resp>(url,this.headers).pipe(
+      delay(1500)
+    );
 
   }
 
