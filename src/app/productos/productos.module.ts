b77d672e1ff5cce858fa1c26fc8f82a3125b8930
JustinAMB <Justinmb@outlook.es>
2022-02-01 01:25:27 -0600

    se importa el modulo de shared

--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -4,15 +4,19 @@ import { CommonModule } from '@angular/common';
 import { ProductosRoutingModule } from './productos-routing.module';
 import { ProductosComponent } from './productos.component';
 import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormProductoComponent } from './form-producto/form-producto.component';
 
 
 @NgModule({
   declarations: [
     ProductosComponent,
-    ListaProductosComponent
+    ListaProductosComponent,
+    FormProductoComponent
   ],
   imports: [
     CommonModule,
+    SharedModule,
     ProductosRoutingModule
   ]
 })
