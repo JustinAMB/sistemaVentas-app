d1fcd50bbb7699c918d81a8af7a1a0ea87b45c40
JustinAMB <Justinmb@outlook.es>
2022-02-01 00:46:33 -0600

    se realiza la logica de la lista de productos

--- src/app/productos/lista-productos/lista-productos.component.ts
+++ src/app/productos/lista-productos/lista-productos.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product } from 'src/app/interfaces/product';
+import { ProductService } from 'src/app/services/product.service';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-lista-productos',
   templateUrl: './lista-productos.component.html',
   styleUrls: ['./lista-productos.component.css']
 })
 export class ListaProductosComponent implements OnInit {
-
-  constructor() { }
+  products!:Product[];
+  load:boolean=true;
+  constructor(private router:Router,private productService:ProductService) { }
+ 
 
   ngOnInit(): void {
+    this.productService.getProducts().subscribe(resp=>{
+      if(resp.ok===true){
+        this.products=resp.data as Product[];
+        this.load=false;
+      }else{
+        this.load=false;
+        Swal.fire({
+          icon:'error',
+          title:'Error',
+          text:'Hubo un error al cargar los productos'
+        });
+        
+      }
+    });
+  }
+  edit (id:number){
+    this.router.navigate(['/productos/editar-producto',id]);
+  }
+
+
+  delete(id:number){
+    Swal.fire({
+      title: 'Estas seguro?',
+      text: "Esta accion no se puede revertir!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Eliminar!'
+    }).then((result) => {
+      if (result.value) { 
+        this.productService.activeProduct(id,false).subscribe(
+          (resp)=>{
+            if(resp.ok===true){
+
+              Swal.fire(
+                'Eliminado!',
+                'El producto ha sido eliminada.',
+                'success'
+              );
+            }
+            else{
+              Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: '¡Algo salió mal! ',
+              
+              });
+            }
+          });
+      }
+    })
   }
 
 }
