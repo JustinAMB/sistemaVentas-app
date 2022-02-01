867e02aa5d7231ac65fffd4db649942231d50a8b
JustinAMB <Justinmb@outlook.es>
2022-01-31 23:50:08 -0600

    se exporta el componente de spinner

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 
 
 @NgModule({
   declarations: [
     NavBarComponent,
-    HeaderComponent
+    HeaderComponent,
+    SpinnerComponent
+
   ],
-  exports: [NavBarComponent,HeaderComponent],
+  exports: [NavBarComponent,HeaderComponent,SpinnerComponent],
   imports: [
     CommonModule
   ]
