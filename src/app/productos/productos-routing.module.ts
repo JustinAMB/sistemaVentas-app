2b4750398936b99441597ae0f2c56366291ad62f
JustinAMB <Justinmb@outlook.es>
2022-02-01 01:24:48 -0600

    se definen las rutas del modulo

--- src/app/productos/productos-routing.module.ts
+++ src/app/productos/productos-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormProductoComponent } from './form-producto/form-producto.component';
 import { ListaProductosComponent } from './lista-productos/lista-productos.component';
+import { ProductosComponent } from './productos.component';
 
 const routes: Routes = [{
-  path: '**',
-  component:ListaProductosComponent
+  path: '',
+  component:ProductosComponent
+},
+{
+  path: 'agregar-producto',
+  component:FormProductoComponent
+},{
+  path: 'editar-producto/:id',
+  component:FormProductoComponent
 }];
 
 @NgModule({
