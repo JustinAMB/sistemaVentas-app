e5608a6ade4596e8c6872f71840433c9443bc4d7
JustinAMB <Justinmb@outlook.es>
2022-02-02 20:18:51 -0600

    se definen las rutas del modulo

--- src/app/proveedores/proveedores-routing.module.ts
+++ src/app/proveedores/proveedores-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormProveedorComponent } from './form-proveedor/form-proveedor.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomeComponent,
+  },{
+    path:'agregar-proveedor',
+    component:FormProveedorComponent
+  },
+  {
+    path:'editar-proveedor/:id',
+    component:FormProveedorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
