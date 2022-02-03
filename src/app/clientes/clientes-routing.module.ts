ce22cf8b84e5d6ac086867d3375113d8436b14c4
JustinAMB <Justinmb@outlook.es>
2022-02-02 23:10:22 -0600

    se definen las rutas del modulo

--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormClienteComponent } from './form-cliente/form-cliente.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent,
+},
+{
+  path: 'agregar-cliente',
+  component: FormClienteComponent},
+{
+  path: 'editar-cliente/:id',
+  component: FormClienteComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
