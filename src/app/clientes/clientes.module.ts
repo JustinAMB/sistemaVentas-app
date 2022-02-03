3641a9cf94acdf42b867013693a6efc860d63f81
JustinAMB <Justinmb@outlook.es>
2022-02-02 23:06:10 -0600

    se importa los modulos necesarios

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientesRoutingModule } from './clientes-routing.module';
+import { SharedModule } from '../shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
     ClientesRoutingModule
   ]
 })
