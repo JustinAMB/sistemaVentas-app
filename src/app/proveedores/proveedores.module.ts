cc478d742ee9a206fea67c29ac1489c5e0975b70
JustinAMB <Justinmb@outlook.es>
2022-02-02 20:17:37 -0600

    se definen las rutas del modulo

--- src/app/proveedores/proveedores.module.ts
+++ src/app/proveedores/proveedores.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProveedoresRoutingModule } from './proveedores-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ListaProveedoresComponent } from './lista-proveedores/lista-proveedores.component';
+import { SharedModule } from '../shared/shared.module';
+import { FormProveedorComponent } from './form-proveedor/form-proveedor.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    ListaProveedoresComponent,
+    FormProveedorComponent
+  ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    SharedModule,
     ProveedoresRoutingModule
   ]
 })
