67c747663d18c5c53fac2ceb75d757a31b65c6c7
JustinAMB <Justinmb@outlook.es>
2022-02-04 00:24:30 -0600

    se elimina la interface de PersonDate y se utiliza la de la carpeta Interface

--- src/app/proveedores/home/home.component.ts
+++ src/app/proveedores/home/home.component.ts
@@ -2,18 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ITable, PdfMakeWrapper, Table, Txt } from 'pdfmake-wrapper';
 import * as pdfFonts from "pdfmake/build/vfs_fonts"; // fonts provided for pdfmake
 import { Person } from 'src/app/interfaces/person';
+import { PersonData } from 'src/app/interfaces/person-data';
 import { PersonService } from 'src/app/services/person.service';
-
-
-interface PersonData{
-
-  name:string;
-  address:string;
-  phone:string;
-  email:string;
-
-}
-
 type TableRow=[string,string,string,string];
 @Component({
   selector: 'app-home',
