3b804fa3940cc1b8e57b34a427a2bf349d92b04d
JustinAMB <Justinmb@outlook.es>
2022-02-06 01:48:27 -0600

    se realiza la logica del formulario

--- src/app/productos/form-producto/form-producto.component.ts
+++ src/app/productos/form-producto/form-producto.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap, tap } from 'rxjs/operators';
+import { Category } from 'src/app/interfaces/category';
+import { CategoryService } from 'src/app/services/category.service';
 import { ProductService } from 'src/app/services/product.service';
 import { UploadService } from 'src/app/services/upload.service';
 
@@ -11,14 +13,24 @@ import { UploadService } from 'src/app/services/upload.service';
   styleUrls: ['./form-producto.component.css']
 })
 export class FormProductoComponent implements OnInit {
-  imag!:string;
+  imgUrl!:string;
   id!:number;
+  categories!:Category[];
+  form:FormGroup=this.fb.group({
+    name:['',[Validators.required,Validators.minLength(3)]],
+    price:['',[Validators.required,Validators.min(0)]],
+    description:['',[Validators.required,Validators.minLength(10)]],
+    category:['',[Validators.required]],
+    inventary_min:[1,[Validators.required,Validators.min(1)]],
+    barcode:['',[Validators.required,Validators.minLength(8)]],
+    unit:['',[Validators.required]],
+  })
   constructor(
     private uploadService: UploadService,
     private fb:FormBuilder,
     private router:Router,
     private activatedRoute: ActivatedRoute,
-    private productService:ProductService) { 
+    private productService:ProductService,private categoryService:CategoryService) { 
    
     
   }
@@ -33,8 +45,94 @@ export class FormProductoComponent implements OnInit {
       tap(params=>{this.id=Number(params['id'])}),
       switchMap(({ id }) =>this.productService.getProduct(this.id))
     ).subscribe(resp=>{
-      console.log(resp);
+      this.form.patchValue(resp.data);
+      this.imgUrl=resp.data.image;
     });
+  }
+  campoNoValido( campo: string ) {
+    return this.form.get(campo)?.invalid
+            && this.form.get(campo)?.touched;
+  }
+  get nameErrorMsg(): string {
+    const errors= this.form.get('name')?.errors;
+    if(errors?.required){
+      return 'Debe ingresar un nombre';
+    } else if(errors?.minlength){
+      return 'El nombre debe de tener como minimo 3 caracteres';
+    }
+    return '';
+  }
+
+  get priceErrorMsg(): string {
+    const errors= this.form.get('price')?.errors;
+    if(errors?.required){
+      return 'Debe ingresar un precio';
+    } else if(errors?.min){
+      return 'El precio debe de ser mayor a 0';
+    }
+    return '';
+  }
+  get descriptionErrorMsg(): string {
+    const errors= this.form.get('description')?.errors;
+    if(errors?.required){
+      return 'Debe ingresar una descripcion';
+    } else if(errors?.minlength){
+      return 'La descripcion debe de tener como minimo 10 caracteres';
+    }
+    return '';
+  }
+  get barcodeErrorMsg(): string {
+    const errors= this.form.get('barcode')?.errors;
+    if(errors?.required){
+      return 'Debe ingresar un codigo de barras';
+    } else if(errors?.minlength){
+      return 'El codigo de barras debe de tener como minimo 8 caracteres';
+    }
+    return '';
+  }
+  get inventaryErrorMsg(): string {
+    const errors= this.form.get('inventary_min')?.errors;
+    if(errors?.required){
+      return 'Debe ingresar un inventario minimo';
+    } else if(errors?.min){
+      return 'El inventario minimo debe de ser mayor a 0';
+    }
+    return '';
+  }
+  get categoryErrorMsg(): string {
+    const errors= this.form.get('category')?.errors;
+
+    if(errors?.required){
+      return 'Debe seleccionar una categoria';
+    }
+    return '';
+  }
+  get unitErrorMsg(): string {
+    const errors= this.form.get('unit')?.errors;
+    if(errors?.required){
+      return 'Debe seleccionar una unidad';
+    }
+    return '';
+  }
+  save() {
+    if(this.form.invalid){
+      return;
+    }
+    const product=this.form.value;
+    product.image=this.imgUrl;
+    console.log(product);
+    if(this.router.url.includes('editar')){
+      product.id=this.id;
+      this.productService.editproduct(product).subscribe(resp=>{
+        console.log(resp);
+        this.router.navigate(['/productos']);
+      });
+    }else{
+      this.productService.addProduct(product).subscribe(resp=>{
+        console.log(resp);
+        this.router.navigate(['/productos']);
+      });
+    }
   }
   upload(event:any) {
     // Get input file
@@ -42,6 +140,7 @@ export class FormProductoComponent implements OnInit {
 
     this.uploadService.upload(file,'products').subscribe(resp=>{
       console.log(resp);
+      this.imgUrl=resp.data;
 
     });
 
