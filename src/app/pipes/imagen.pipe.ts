98d2bfba418f280b75cbf96adffd9303939af5f8
JustinAMB <Justinmb@outlook.es>
2022-02-06 01:52:27 -0600

    se modifica las url de return

--- src/app/pipes/imagen.pipe.ts
+++ src/app/pipes/imagen.pipe.ts
@@ -7,13 +7,14 @@ import { environment } from 'src/environments/environment';
 export class ImagenPipe implements PipeTransform {
 
   transform( img: string, tipo: 'users'|'products'): string {
+    console.log(img);
     const api=environment.api;
       if ( !img ) {
-        return `${ api}/upload/usuarios/no-image`;
+        return `${ api}upload/usuarios/no-image`;
     } else if ( img.includes('https') ) {
         return img;
     } else {
-        return `${ api}/upload/${ tipo }/${ img }`;
+        return `${ api}upload/${ tipo }/${ img }`;
     } 
 
 
