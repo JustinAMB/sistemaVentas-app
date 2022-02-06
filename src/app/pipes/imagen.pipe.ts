1efc9717c2991688f3dd685b7454fc659e6e4fa7
JustinAMB <Justinmb@outlook.es>
2022-02-05 23:55:12 -0600

    se realiza un pipe para manejar las url de las imagenes

---
+++ src/app/pipes/imagen.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Pipe({
+  name: 'imagen'
+})
+export class ImagenPipe implements PipeTransform {
+
+  transform( img: string, tipo: 'users'|'products'): string {
+    const api=environment.api;
+      if ( !img ) {
+        return `${ api}/upload/usuarios/no-image`;
+    } else if ( img.includes('https') ) {
+        return img;
+    } else {
+        return `${ api}/upload/${ tipo }/${ img }`;
+    } 
+
+
+  }
+
+}
