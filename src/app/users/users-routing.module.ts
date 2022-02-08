ffad3160cf318fee3d537901b6a60dbf3ccd6099
JustinAMB <Justinmb@outlook.es>
2022-02-07 23:14:01 -0600

    se definen las rutas del modulo

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormUserComponent } from './form-user/form-user.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: '',
+  component:HomeComponent,
+},
+{
+  path:'agregar-usuario',
+  component:FormUserComponent
+},{
+  path:'editar-usuario/:id',
+  component:FormUserComponent
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
