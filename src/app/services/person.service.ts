430f58298b22b8c6dc06c13226f3769c13721462
JustinAMB <Justinmb@outlook.es>
2022-02-09 23:26:29 -0600

    se agrega la funciones el catch por los errores

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, delay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Person } from '../interfaces/person';
 import { Resp } from '../interfaces/resp';
@@ -29,27 +30,60 @@ export class PersonService {
 
   addPerson(kind:number,person:Person):Observable<Resp>{
     const url=`${this.baseUrl}person/${kind}`;
-    return this.http.post<Resp>(url,{...person},this.headers);
+    return this.http.post<Resp>(url,{...person},this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
+      delay(1500)
+    );
 
   }
   editPerson(id:number,person:Person):Observable<Resp>{
     const url=`${this.baseUrl}person/${id}`;
-    return this.http.put<Resp>(url,{...person},this.headers);
+    return this.http.put<Resp>(url,{...person},this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
+      delay(1500)
+    );
 
   }
   
   getPerson(id:number):Observable<Resp>{
     const url=`${this.baseUrl}person/${id}`;
-    return this.http.get<Resp>(url,this.headers);
+    return this.http.get<Resp>(url,this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  
+    );
   }
   getPersons(kind:number,state:boolean=true):Observable<Resp>{
     const url=`${this.baseUrl}person/getPersons/${kind}?state=${state}`;
-    return this.http.get<Resp>(url,this.headers);
+    return this.http.get<Resp>(url,this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
+      delay(1000)
+    );
   }
 
   activePerson(id:number,state:boolean):Observable<Resp> {
     const url=`${this.baseUrl}person/active/${id}`; 
-    return this.http.put<Resp>(url,{state},this.headers);
+    return this.http.put<Resp>(url,{state},this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
+      delay(1500)
+    );
 
   }
 }
