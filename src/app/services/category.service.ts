7d23f3470cef1bcd2e583aae5a4858e2b45eafc5
JustinAMB <Justinmb@outlook.es>
2022-02-09 23:24:50 -0600

    se agrega la funciones el catch por los errores

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { catchError, delay } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Category } from '../interfaces/category';
 import { Resp } from '../interfaces/resp';
@@ -27,30 +27,62 @@ export class CategoryService {
   }
   addCategory(category:Category):Observable<Resp> {
     const url=`${this.baseUrl}category/`; 
-    return this.http.post<Resp>(url,{...category},this.headers);
+    return this.http.post<Resp>(url,{...category},this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
+      delay(1500)
+    );
 
   }
 
   editCategory(category:Category):Observable<Resp> {
     const url=`${this.baseUrl}category/${category.id}`; 
-    return this.http.put<Resp>(url,{...category},this.headers);
+    return this.http.put<Resp>(url,{...category},this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
+      delay(1500)
+    );
 
   }
   activeCategory(id:number,state:boolean):Observable<Resp> {
     const url=`${this.baseUrl}category/active/${id}`; 
-    return this.http.put<Resp>(url,{state},this.headers);
+    return this.http.put<Resp>(url,{state},this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
+      delay(1500)
+    );
 
   }
 
   getCategory(id:number):Observable<Resp> {
     const url=`${this.baseUrl}category/${id}`; 
-    return this.http.get<Resp>(url,this.headers);
+    return this.http.get<Resp>(url,this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  
+    );
+
 
   }
   getCategorys(state:boolean):Observable<Resp> {
 
     const url=`${this.baseUrl}category/?state=${state}`; 
     return this.http.get<Resp>(url,this.headers).pipe(
+      catchError(err=>{
+        console.log(err);
+        return of(err.error)
+      })  ,
+
       delay(1500)
     );
 
