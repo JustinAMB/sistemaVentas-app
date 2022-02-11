7d92225edd5813998c5f1a5ec862d455522d1e36
JustinAMB <Justinmb@outlook.es>
2022-02-11 00:48:20 -0600

    se realiza la configuracion del spinner

--- src/app/inventario/home/home.component.ts
+++ src/app/inventario/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/product.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -10,14 +10,28 @@ import { ProductService } from 'src/app/services/product.service';
 export class HomeComponent implements OnInit {
   products!:Product[];
   productS:string='';
+  load:boolean=true;
   constructor(private productService:ProductService) { }
 
   ngOnInit(): void {
     this.getProduct();
   }
   getProduct(){
+    this.load=true;
     this.productService.searchInventary(this.productS).subscribe(resp=>{
-      this.products=resp.data;
+      if(resp.ok===true){
+        this.products=resp.data;
+        this.load=false;
+      }else{
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'No se encontraron resultados',
+        });
+        this.load=false;
+      }
+     
+      
     });
   }
   buscar(){
