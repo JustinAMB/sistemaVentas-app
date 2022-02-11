8bf0d9d2d4076a8762efeea9e56a091ca75dafa0
JustinAMB <Justinmb@outlook.es>
2022-02-10 23:32:35 -0600

    se realiza la logica de la lista

---
+++ src/app/inventario/lista-inventario/lista-inventario.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+
+@Component({
+  selector: 'app-lista-inventario',
+  templateUrl: './lista-inventario.component.html',
+  styleUrls: ['./lista-inventario.component.css']
+})
+export class ListaInventarioComponent implements OnInit {
+  @Input() productos!: Product[];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
