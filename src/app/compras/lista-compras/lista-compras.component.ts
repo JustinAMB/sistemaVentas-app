05f2f92b282fee3008e04f5e5415cb8764c6a59d
JustinAMB <Justinmb@outlook.es>
2022-02-12 00:52:30 -0600

    se realiza la logica de la lista

---
+++ src/app/compras/lista-compras/lista-compras.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SellDetail } from 'src/app/interfaces/sell-detail';
+import { SellDetailsService } from 'src/app/services/sell-details.service';
+
+@Component({
+  selector: 'app-lista-compras',
+  templateUrl: './lista-compras.component.html',
+  styleUrls: ['./lista-compras.component.css']
+})
+export class ListaComprasComponent implements OnInit {
+
+  constructor(private detailsService:SellDetailsService) { }
+
+  ngOnInit(): void {
+  }
+  get details(): SellDetail[] {
+    return this.detailsService.details;
+  }
+
+}
