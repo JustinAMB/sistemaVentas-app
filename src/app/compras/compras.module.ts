b10ba81d85d59f73760398190ca687ea8f55a2e3
JustinAMB <Justinmb@outlook.es>
2022-02-13 23:54:37 -0600

    se agregan modulos necesarios

--- src/app/compras/compras.module.ts
+++ src/app/compras/compras.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { ComprasRoutingModule } from './compras-routing.module';
 import { HomeComponent } from './home/home.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { FormDetalleComponent } from './form-detalle/form-detalle.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    FormDetalleComponent
+    FormDetalleComponent,
+    ListaComprasComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
+    FormsModule,
     ReactiveFormsModule,
     ComprasRoutingModule
   ]
