ffdf4d1a6b369d44b485857f4f8dbfbba1ce4866
JustinAMB <Justinmb@outlook.es>
2022-02-13 21:02:15 -0600

    se realiza la logica del metodo comprar

--- src/app/compras/home/home.component.ts
+++ src/app/compras/home/home.component.ts
@@ -1,30 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SellDetail } from 'src/app/interfaces/sell-detail';
+import { SellDetailsService } from 'src/app/services/sell-details.service';
 import { SellService } from 'src/app/services/sell.service';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  details:SellDetail[]=[];
-  form:FormGroup=this.fb.group({
-    quatity:[0,[Validators.required,Validators.min(1)]],
-    priceUnit:[0,[Validators.required,Validators.min(1)]],
-    barcode:[],
+  correo!:string;
+
   
-  })
-  constructor(private sellService:SellService,private fb:FormBuilder) { }
+  constructor(private sellService:SellService,private detailsService:SellDetailsService) { }
 
   ngOnInit(): void {
   }
-  addDetail(detail:SellDetail){
-    this.details.push(detail);
+  get total():number{
+    return this.detailsService.total;
   }
-  deleteDetail(index:number){
-    this.details.splice(index,1);
-  } 
+
+  comprar(){	
+    this.sellService.addSell(1,this.total,this.correo,this.detailsService.details).subscribe((resp)=>{
+      Swal.fire('Compra realizada','','success');
+
+    });
+
+  }
 
 }
