025268e886538f48297cc8bae19259982dfe0073
JustinAMB <Justinmb@outlook.es>
2022-02-14 21:19:36 -0600

    se implementan las funciones de validacion

--- src/app/compras/home/home.component.ts
+++ src/app/compras/home/home.component.ts
@@ -10,23 +10,45 @@ import Swal from 'sweetalert2';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  correo!:string;
+  form:FormGroup=this.fb.group({
+    correo:['',[Validators.required,Validators.email]]
+  });
 
   
-  constructor(private sellService:SellService,private detailsService:SellDetailsService) { }
+  constructor(private sellService:SellService,private detailsService:SellDetailsService,private fb:FormBuilder) { }
 
   ngOnInit(): void {
   }
   get total():number[]{
     return this.detailsService.total;
   }
+  campoValido( campo: string ) {
+    return  this.form.get(campo)?.invalid
+    && this.form.get(campo)?.touched;
+  }
 
+  get correoMsgError(): string {
+    const errors= this.form.get('correo')?.errors;
+    if(errors?.required){
+      return 'Debe ingresar un correo';
+    } else if(errors?.email){
+      return 'Debe ingresar un correo válido';
+    }
+    return '';
+  }
+  get isDisabled(): boolean {
+    return this.detailsService.details.length === 0 || this.form.invalid;
+  }
   comprar(){	
-    this.sellService.addSell(1,this.total[2],this.correo,this.detailsService.details).subscribe((resp)=>{
-      Swal.fire('Compra realizada','','success');
-
-    });
-
+    this.form.markAllAsTouched();
+    if(this.form.valid){
+      const {correo}=this.form.value;
+      this.sellService.addSell(1,this.total[2],correo,this.detailsService.details).subscribe((resp)=>{
+        Swal.fire('Compra realizada','','success');
+  
+      });
+    }
+    
   }
   cancelar(){
     this.detailsService.clear();
