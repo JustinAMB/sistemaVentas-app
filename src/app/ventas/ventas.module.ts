72ac27fd44ccfc8b78d108295ca83fc8105b5492
JustinAMB <Justinmb@outlook.es>
2022-02-15 17:05:41 -0600

    se importan los moduloss necesarios

--- src/app/ventas/ventas.module.ts
+++ src/app/ventas/ventas.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VentasRoutingModule } from './ventas-routing.module';
+import { HomeComponent } from './home/home.component';
+import { ListaDetallesComponent } from './lista-detalles/lista-detalles.component';
+import { FormDetalleComponent } from './form-detalle/form-detalle.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    ListaDetallesComponent,
+    FormDetalleComponent
+  ],
   imports: [
     CommonModule,
+    SharedModule,
+    ReactiveFormsModule,
     VentasRoutingModule
   ]
 })
