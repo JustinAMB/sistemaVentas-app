9e403e5252f65500476dda64218ff1abae781b7f
JustinAMB <Justinmb@outlook.es>
2022-02-15 18:10:30 -0600

    crea la estructura html del componente

---
+++ src/app/ventas/form-detalle/form-detalle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-detalle',
+  templateUrl: './form-detalle.component.html',
+  styleUrls: ['./form-detalle.component.css']
+})
+export class FormDetalleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
