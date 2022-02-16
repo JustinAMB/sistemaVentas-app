e6b7611c9bd59dc08d06d26f59dcbbc0fa834e23
JustinAMB <Justinmb@outlook.es>
2022-02-15 21:01:46 -0600

    se crea la logica del componente

---
+++ src/app/ventas/home/home.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SellDetailsService } from 'src/app/services/sell-details.service';
+import { SellService } from 'src/app/services/sell.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  form:FormGroup=this.fb.group({
+    correo:['',[Validators.required,Validators.email]]
+  });
+
+  
+  constructor(
+    private sellService:SellService,
+    private detailsService:SellDetailsService,
+    private fb:FormBuilder) { }
+
+  ngOnInit(): void {
+  }
+  get total():number[]{
+    return this.detailsService.total;
+  }
+  campoValido( campo: string ) {
+    return  this.form.get(campo)?.invalid
+    && this.form.get(campo)?.touched;
+  }
+
+  get correoMsgError(): string {
+    const errors= this.form.get('correo')?.errors;
+    if(errors?.required){
+      return 'Debe ingresar un correo';
+    } else if(errors?.email){
+      return 'Debe ingresar un correo válido';
+    }
+    return '';
+  }
+  get isDisabled(): boolean {
+    return this.detailsService.details.length === 0 || this.form.invalid;
+  }
+  vender(){	
+    this.form.markAllAsTouched();
+    if(!this.isDisabled){
+      const {correo}=this.form.value;
+      this.sellService.addSell(1,this.total[2],correo,this.detailsService.details).subscribe((resp)=>{
+        if(resp.ok===true){
+          Swal.fire('Venta realizada','','success');
+        }
+        else
+        {
+          Swal.fire('Error',resp.message,'error');
+        }
+        
+  
+      });
+    }
+    
+  }
+  cancelar(){
+    this.detailsService.clear();
+  }
+
+}
