5b0f30d1e58418f188006fd4344a8d83174280ec
JustinAMB <Justinmb@outlook.es>
2022-02-17 15:27:24 -0600

    crea la logica del component

---
+++ src/app/caja/lista-sells/lista-sells.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Sell } from 'src/app/interfaces/sell';
+import { SellService } from 'src/app/services/sell.service';
+
+@Component({
+  selector: 'app-lista-sells',
+  templateUrl: './lista-sells.component.html',
+  styleUrls: ['./lista-sells.component.css']
+})
+export class ListaSellsComponent implements OnInit {
+  sells!:Sell[];
+  load:boolean=true;
+  constructor(private sellService: SellService, private router:Router) { }
+
+  ngOnInit(): void {
+    this.sellService.getSellsByDay().subscribe(resp=>{
+      this.sells=resp.data;
+      this.load=false;
+    });
+
+  }
+  ver(id:number):void {
+    this.router.navigate(['/caja/ver-detalle',id]);
+  }
+
+}
