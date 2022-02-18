979121b47a95d0fab39b02577ace70920769eb02
JustinAMB <Justinmb@outlook.es>
2022-02-18 01:12:52 -0600

    se realiza la carga de person

--- src/app/caja/view-sell/view-sell.component.ts
+++ src/app/caja/view-sell/view-sell.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
+import { Person } from 'src/app/interfaces/person';
 import { Sell } from 'src/app/interfaces/sell';
 import { SellDetail } from 'src/app/interfaces/sell-detail';
+import { PersonService } from 'src/app/services/person.service';
 import { SellService } from 'src/app/services/sell.service';
 
 @Component({
@@ -12,8 +14,9 @@ import { SellService } from 'src/app/services/sell.service';
 })
 export class ViewSellComponent implements OnInit {
   details!:SellDetail[];
+  person!:Person;
   sell!:Sell;
-  constructor(private sellService: SellService,private activatedRoute: ActivatedRoute) { }
+  constructor(private sellService: SellService,private activatedRoute: ActivatedRoute,private personService: PersonService) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.pipe(
@@ -21,7 +24,16 @@ export class ViewSellComponent implements OnInit {
     ).subscribe(resp=>{
       this.sell=resp.data.sell ;
       this.details=resp.data.details;
+      this.personService.getPerson(this.sell.person!).subscribe(resp=>{
+        this.person=resp.data;
+      });
     });
+    
+    
   }
-
+  getDate(fecha:Date):string{
+    
+    const hoy = new Date(fecha);
+    return hoy.toLocaleString().split(',').join('');
+  }
 }
