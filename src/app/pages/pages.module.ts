8f5bdcc1fd744d3cfc1426be3bc9d2c2d915f24e
JustinAMB <Justinmb@outlook.es>
2022-02-20 22:41:40 -0600

    se importan modulos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,18 +5,31 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages/pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReportsComponent } from './reports/reports.component';
+import { ChartsModule } from 'ng2-charts';
+import { ProductsTopComponent } from './products-top/products-top.component';
 
+import { ProductsCategoriesComponent } from './products-categories/products-categories.component';
+import { SellDaysComponent } from './sell-days/sell-days.component';
+import { ReportsGeneralComponent } from './reports-general/reports-general.component';
 @NgModule({
   declarations: [
-    PagesComponent
+    PagesComponent,
+    ReportsComponent,
+    ProductsTopComponent,
+ 
+    ProductsCategoriesComponent,
+      SellDaysComponent,
+      ReportsGeneralComponent
   ],
   imports: [
     CommonModule,
     SharedModule,
+    
     RouterModule,    
     BrowserModule ,
     PagesRoutingModule,
-    
+   ChartsModule 
   ]
 })
 export class PagesModule { }
