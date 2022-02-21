87feb74037c92cb2984f54e445dfe78507c4be88
JustinAMB <Justinmb@outlook.es>
2022-02-20 22:40:55 -0600

    e importan modulos

--- src/app/compras/compras.module.ts
+++ src/app/compras/compras.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ComprasRoutingModule } from './compras-routing.module';
 import { HomeComponent } from './home/home.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { FormDetalleComponent } from './form-detalle/form-detalle.component';
 import { ListaComprasComponent } from './lista-compras/lista-compras.component';
@@ -18,7 +18,6 @@ import { ListaComprasComponent } from './lista-compras/lista-compras.component';
   imports: [
     CommonModule,
     SharedModule,
-    FormsModule,
     ReactiveFormsModule,
     ComprasRoutingModule
   ]
