ebc25fb2e2df4c3966826d78b1d45d45a1fa1769
JustinAMB <Justinmb@outlook.es>
2022-03-09 23:24:35 -0600

    se importa el modulo de paginacion

--- src/app/inventario/inventario.module.ts
+++ src/app/inventario/inventario.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { InventarioRoutingModule } from './inventario-routing.module';
 import { HomeComponent } from './home/home.component';
 import {  FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { ListaInventarioComponent } from './lista-inventario/lista-inventario.component';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 
 @NgModule({
@@ -15,6 +15,7 @@ import { ListaInventarioComponent } from './lista-inventario/lista-inventario.co
   ],
   imports: [
     CommonModule,
+    NgxPaginationModule,
     FormsModule,
     SharedModule,
     InventarioRoutingModule
