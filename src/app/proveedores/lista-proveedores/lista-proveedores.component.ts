28a4e5584f3aa2527263a0ef811863483bb75994
JustinAMB <Justinmb@outlook.es>
2022-03-09 23:53:06 -0600

    se realiza la paginacion de la lista

--- src/app/proveedores/lista-proveedores/lista-proveedores.component.ts
+++ src/app/proveedores/lista-proveedores/lista-proveedores.component.ts
@@ -12,6 +12,7 @@ import Swal from 'sweetalert2';
 })
 export class ListaProveedoresComponent implements OnInit {
   @Input() proveedores!:Person[] ;
+  current:number=10;
   
   constructor(private router:Router, private personService:PersonService) { }
 
