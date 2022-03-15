fd3c4e515a0bb812954bb2713d42d01c69e417b8
JustinAMB <Justinmb@outlook.es>
2022-03-15 01:07:20 -0600

    se hace el layout responseive

--- src/app/pages/pages/pages.component.ts
+++ src/app/pages/pages/pages.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/interfaces/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { ResponsiveService } from 'src/app/services/responsive.service';
 
 @Component({
   selector: 'app-pages',
@@ -11,8 +12,13 @@ export class PagesComponent implements OnInit {
   get auth():User{
     return this.authService.user;
   }
-  constructor(private authService:AuthService) { }
-
+  constructor(private authService:AuthService,private responsive:ResponsiveService) { }
+  get tam():boolean{
+    return window.innerWidth<768;
+  }
+  get navbar():boolean{
+    return this.responsive.navbar;
+  }
   ngOnInit(): void {
   }
 
