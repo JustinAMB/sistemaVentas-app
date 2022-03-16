04f55a8d395793280620e8290d28fdb8e104fc9b
JustinAMB <Justinmb@outlook.es>
2022-03-15 21:50:47 -0600

    se realiza el routeo al menu

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -8,7 +8,54 @@ import { ResponsiveService } from 'src/app/services/responsive.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  items=[
+    {
+      name:"Inicio",
+      route:"/",
+      icon:'uil uil-house-user'
+    },
+    {
+      name:"Ventas",
+      route:"/ventas",
+      icon:'uil uil-shopping-cart'
+    },{
+      name:"Caja",
+      route:"/caja",
+      icon:'uil uil-cash-register'
+    },{
+      name:"Compras",
+      route:"/compras",
+      icon:'uil uil-invoice'
+    },{
+      name:"Categorias",
+      route:"/categorias",
+      icon:'uil uil-label'
+    },{
+      name:"Productos",
+      route:"/productos",
+      icon:'uil uil-cube'
+    },{
+      name:"Proveedores",
+      route:"/proveedores",
+      icon:'uil uil-truck'
+    },
+    {
+      name:"Clientes",
+      route:"/clientes",
+      icon:'uil uil-users-alt'
+    },{
+      name:"Inventario",
+      route:"/inventario",
+      icon:'uil uil-comparison'
+    },
+    {
+      name:"Usuarios",
+      route:"/usuarios",
+      icon:'uil uil-user-md'
+    }
 
+
+  ];
   constructor(private router:Router,private responsive:ResponsiveService) { }
   get navbar():boolean{
     return this.responsive.navbar;
@@ -16,8 +63,8 @@ export class NavBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  navegar(ruta:string){
-    this.router.navigate([ruta]);
+  navegar(route:string){
+    this.router.navigate(['/sistema/'+route]);
   }
   cambiarNavbar(){
     this.responsive.navbar=!this.responsive.navbar;
