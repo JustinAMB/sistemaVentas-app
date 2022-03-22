3973ef8b33520f63bfe7f736d7e21864aa8bda97
JustinAMB <Justinmb@outlook.es>
2022-03-21 23:00:34 -0600

    se arega un observable para el ancho de la pantalla

--- src/app/services/responsive.service.ts
+++ src/app/services/responsive.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResponsiveService {
+  resizeObservable$: Observable<Event>;
+resizeSubscription$: Subscription;
+
   navbar:boolean=false;
-  constructor() { }
+  tam:boolean=true;
+  constructor() { 
+    this.resizeObservable$ = fromEvent(window, 'resize')
+    this.resizeSubscription$ = this.resizeObservable$.subscribe( evt => {
+      this.tam=window.innerWidth<768;
+      this.navbar=(window.innerWidth<768)  && (!this.navbar);
+    })
+    
+  }
 }
